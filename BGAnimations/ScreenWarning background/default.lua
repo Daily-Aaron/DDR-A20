7761e32223a1e32120140e16c6a0e11f1689d28e
Curilang <curilang30@gmail.com>
2021-08-10 03:47:19 -0500

    NEW BG

--- BGAnimations/ScreenWarning background/default.lua
+++ BGAnimations/ScreenWarning background/default.lua
@@ -1,42 +1,17 @@
-local t = Def.ActorFrame {};
+return Def.ActorFrame {
+	Def.Quad{
+		InitCommand=function(s) s:diffuse(color("#CC0000"))
+		:setsize(SCREEN_WIDTH,SCREEN_HEIGHT):Center()
+		:sleep(1):linear(0.25):diffusealpha(1) end,
+	};
 
---[[local lang = "en";
-if THEME:GetCurLanguage() == "ja" then
-	lang = "jp";
-elseif THEME:GetCurLanguage() == "kor" then
-	lang = "kor";
-end;]]
+	LoadActor(version..ddrgame..lang.."_background")..{
+		InitCommand=function(s) s:FullScreen():sleep(3):linear(0.25):diffusealpha(0) end,
+	};
 
-t[#t+1] = Def.ActorFrame {
-  InitCommand=cmd();
-	Def.ActorFrame {
-		Def.Quad{
-			InitCommand=cmd(diffuse,color("#CC0000");setsize,SCREEN_WIDTH,SCREEN_HEIGHT;Center;sleep,1;linear,0.25;diffusealpha,1;);
-		};
-	}
+	Def.Quad{
+		InitCommand=function(s) s:diffuse(Color("White"))
+		:setsize(SCREEN_WIDTH,SCREEN_HEIGHT):Center()
+		:sleep(0.5):linear(0.25):diffusealpha(0) end,
+	};
 };
-
-if ThemePrefs.Get("Version") == "A20" then
-t[#t+1] = Def.ActorFrame {
-  InitCommand=cmd();
-		LoadActor(ddrgame..lang.."_background")..{
-			InitCommand=cmd(FullScreen;sleep,3;linear,0.25;diffusealpha,0;);
-		};
-};
-else
-t[#t+1] = Def.ActorFrame {
-  InitCommand=cmd();
-		LoadActor("a20+_"..ddrgame.."background")..{
-			InitCommand=cmd(FullScreen;sleep,3;linear,0.25;diffusealpha,0;);
-		};
-};
-end;
-
-t[#t+1] = Def.ActorFrame {
-  InitCommand=cmd();
-		Def.Quad{
-			InitCommand=cmd(diffuse,Color("White");setsize,SCREEN_WIDTH,SCREEN_HEIGHT;Center;sleep,0.5;linear,0.25;diffusealpha,0;);
-		};
-};
-	
-return t
